ecc60f3835f71138df28524b72391cbff076998c
akhil <4khiln@gmail.com>
2024-11-29 15:30:43 +0530

    feat: add mentor-related components and enums
    - Introduced  enum to centralize mentor-specific URLs.
    - Added  component for the mentor navigation bar.
    - Created  for the mentor landing experience.

---
+++ src/@types/enums/MentorUrl.ts
@@ -0,0 +1,8 @@
+export enum MentorUrl{
+    home = '/mentor',
+    signIn = '/mentor/signin',
+    signUp = '/mentor/signup',
+    otp = '/mentor/otp/:id',
+    signOut = '/mentor/signout',
+    profile = '/mentor/profile'
+}
---
+++ src/pages/MentorLandingPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MentorLandingPage() {
+  return (
+    <div className='w-full '>
+        
+    </div>
+  )
+}
+
+export default MentorLandingPage
