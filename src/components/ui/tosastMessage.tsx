cd6e2b12811aac82cd4bee1e6ab8aa66b0b6bf02
akhil <4khiln@gmail.com>
2024-12-07 09:16:28 +0530

    style: format code for better readability and consistency

--- src/components/ui/tosastMessage.tsx
+++ src/components/ui/tosastMessage.tsx
@@ -1,33 +1,29 @@
 import { toast } from "@/hooks/use-toast";
 import { CircleCheckBig } from "lucide-react";
 
-
-export const AuthUnexpectedErrorTost =  () => toast({
+export const AuthUnexpectedErrorTost = () =>
+  toast({
     title: "An unexpected error occurred",
     description: "Please try again later.",
     className: "text-red-700 border bg-gray-200 mb-6",
-});
+  });
 
-
-export const errorTost = ( title:string , description:string )=> {
-    console.log("dddd");
-    
-    toast({
-    
-    title: title ,
+export const errorTost = (title: string, description: string) =>
+  toast({
+    title: title,
     description: description,
     className: "text-red-700 border bg-gray-200 mb-6",
-});}
+  });
 
 export const successToast = (title: string, description: string) => {
-    
-    toast({
-        title: (
-            <div className="flex ">
-                <CircleCheckBig size={15} className="mr-2 mt-0.5"/>{title } 
-            </div>
-        ),
-        description: description,
-        className: "text-green-700 border bg-gray-200 mb-6",
-    });
+  toast({
+    title: (
+      <div className="flex ">
+        <CircleCheckBig size={15} className="mr-2 mt-0.5" />
+        {title}
+      </div>
+    ),
+    description: description,
+    className: "text-green-700 border bg-gray-200 mb-6",
+  });
 };
