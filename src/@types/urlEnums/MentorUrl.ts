bd7a1ad57714ba9c929c0bc12ba7feab9979b5ed
akhil <4khiln@gmail.com>
2024-12-11 12:06:39 +0530

    feat: update MentorUrl enum with new routes
    - Added 'forgotPassword' and 'setNewPassword' routes to MentorUrl enum.
    - Modified package-lock.json and package.json to reflect dependencies updates.

--- src/@types/urlEnums/MentorUrl.ts
+++ src/@types/urlEnums/MentorUrl.ts
@@ -2,8 +2,10 @@ export enum MentorUrl{
     home = '/mentor',
     signIn = '/mentor/signin',
     signUp = '/mentor/signup',
-    otp = '/mentor/otp/:id',
+    otp = '/mentor/otp',
     signOut = '/mentor/signout',
+    forgotPassword = '/mentor/forgotpassword',
+    setNewPassword = '/mentor/resetpassword/:token',
     profile = '/mentor/profile',
     subscription = '/mentor/subscription'
 }
