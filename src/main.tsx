9eee304589c25b55b3f81a780731e977eea3cb31
akhil <4khiln@gmail.com>
2025-02-06 20:19:54 +0530

    feat: integrate Sentry for error tracking and monitoring
    - Added Sentry initialization in
    - Configured Sentry settings in
    - Updated dependencies in  and
    - Modified  to exclude relevant files if needed

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,33 @@
-import { createRoot } from 'react-dom/client';
-import '@/assets/styles/index.css';
-import App from './App' ;
-import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { injectStore } from './services/axios/axiosInstance';
+import { createRoot } from "react-dom/client";
+import "@/assets/styles/index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { injectStore } from "./services/axios/axiosInstance";
 injectStore(store);
 
-createRoot(document.getElementById('root')!).render(
-  <Provider store={store} >
-    < PersistGate loading={null} persistor={persistor}>
-    <App />
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://977df4528040acbeebfef2f8b37f9e03@o4508772597039104.ingest.us.sentry.io/4508772653924352",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
     </PersistGate>
-  </Provider>,
-)
+  </Provider>
+);
