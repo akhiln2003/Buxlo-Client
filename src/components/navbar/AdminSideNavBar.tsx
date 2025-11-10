2b9267ad0fffd91d3faa0b6d8a445a8f535bc583
akhil <4khiln@gmail.com>
2025-11-10 15:50:57 +0530

    refactor: update admin sidebar and layout components

--- src/components/navbar/AdminSideNavBar.tsx
+++ src/components/navbar/AdminSideNavBar.tsx
@@ -7,7 +7,7 @@ function AdminSideNavBar() {
   const {pathname} = useLocation();
   
   return (
-    <div className="w-20  dark:bg-zinc-950 hidden md:flex flex-col  items-center space-y-8 pt-[1.5rem]  ">
+    <div className="w-20 mt-10  dark:bg-zinc-950 hidden md:flex flex-col  items-center space-y-8 pt-[1.5rem]  ">
       <Link to={AdminUrls.dashboard} className="flex-col items-center h-[3rem] w-3/4 mt-6  text-center ">
       <div className={`h-5/6 mx-[0.5rem] rounded-xl flex justify-center items-center ${ pathname == AdminUrls.dashboard  ? 'bg-zinc-300 dark:bg-zinc-900':  'hover:bg-zinc-50 dark:hover:bg-zinc-700'} `}><House /></div>
         <p className="text-xs font-cabinet ">Dashboard</p>
